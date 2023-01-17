2a15cc87d9faa773b46c172695050a863b3a23f9
Nur <dummy@mail>
2023-01-17 16:46:55 +0530

    done

--- multiplication_method.py
+++ multiplication_method.py
@@ -2,9 +2,9 @@
 from math import sqrt
 def hash(k,m):
     A = (sqrt(5)-1)/2
-    hash = (int(m) * int(k) * A) % 1
-    return str(hash)
+    hash = ( int(m) * (int(k) * A % 1) ) / 1
+    return str(int(hash))
 
 array_size = input("Enter the size of Array:")
 k = input("Enter Key:")
-print("Index will be:" + hash(k,array_size))
+print("Index will be:" + hash(k, array_size))
