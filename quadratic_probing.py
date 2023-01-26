bf7fbdd127ef4db73d071f4c2eaacb9759f56a78
Nur Aktar <sknuraktar1999@gmail.com>
2023-01-26 14:02:34 +0530

    init

---
+++ quadratic_probing.py
@@ -0,0 +1,24 @@
+#Quadratic Probing
+array = [-1]*10
+notfull = 0
+def hash(k,m):
+    global notfull
+    i = 0
+    c1 = 1
+    c2 = 3
+    while i<10:
+        hash = (k % m +c1*i+c2*i*i) % m
+        if array[hash] == -1:
+            array[hash] = k
+            notfull =0
+            break
+        notfull = 1
+        i += 1
+    return 0
+
+m = int(input("Enter array size:"))
+while notfull != 1:
+    k = int(input("Enter key value:"))
+    hash(k,m)
+
+print(array)
